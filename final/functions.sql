c2c2b0819ac4fa8fea2a1184b32e38234506fc01
bergconnor <cberg1@ksu.edu>
2016-12-11 14:33:20 -0600

    a bunch of shit:
    updated shows table unique constraint
    updated functions and triggers for handling show titles
    added recommendations feature

--- final/functions.sql
+++ final/functions.sql
@@ -1,10 +1,22 @@
-DELIMITER $$
-CREATE FUNCTION get_subscription_remainder(date start, date end)
-RETURNS int
-declare restdays interval
-begin
-	restdays := enddate - startdate;
-	return restdays; 
+/* Delete the functions if they already exist */
+DROP FUNCTION IF EXISTS display_name;
 
-end;
-$$ Language plpgsql;
+/* Create the schema for our functions */
+DELIMITER $$
+
+CREATE FUNCTION display_name(old_name varchar(100))
+RETURNS varchar(100)
+    DETERMINISTIC
+BEGIN
+    RETURN 
+    CASE 
+        WHEN ((position('(T' IN old_name) = 0) AND (position('(A' IN old_name) = 0)) THEN
+            old_name
+        ELSE
+            trim(concat(trim(')' FROM substr(old_name, position('(' IN old_name) + 1)), ' ',
+                                  trim(substr(old_name, 1, position('(' IN old_name) - 1))))
+    END;
+END;
+$$
+
+DELIMITER ;
--- final/triggers.sql
+++ final/triggers.sql
@@ -1,6 +1,10 @@
-DROP TRIGGER  IF EXISTS user_trig;
+/* Delete the triggers if they already exist */
+DROP TRIGGER IF EXISTS user_trig;
+DROP TRIGGER IF EXISTS title_trig;
 
+/* Create the schema for our triggers */
 DELIMITER $$
+
 CREATE TRIGGER user_trig BEFORE INSERT ON users
     FOR EACH ROW
     BEGIN
@@ -9,4 +13,16 @@ CREATE TRIGGER user_trig BEFORE INSERT ON users
         END IF;
     END;
 $$
+
+CREATE TRIGGER display_name BEFORE INSERT ON shows
+    FOR EACH ROW
+    BEGIN
+        IF (strcmp(substring_index(NEW.name, ' ', 1), 'The') = 0) THEN
+            SET NEW.name = trim(concat(substring(NEW.name, locate(' ', NEW.name)), ' (The)'));
+        ELSEIF (strcmp(substring_index(NEW.name, ' ', 1), 'A') = 0) THEN
+            SET NEW.name = trim(concat(substring(NEW.name, locate(' ', NEW.name)), ' (A)'));
+        END IF;
+    END;
+$$
+
 DELIMITER ;
